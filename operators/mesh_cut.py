b5f725fe6a43a064b1fba90b1959ef148aaf0f72
machin3io <social@machin3.io>
2022-12-11 13:49:01 +0100

    MeshCut() - support redoing

--- operators/mesh_cut.py
+++ operators/mesh_cut.py
@@ -1,78 +1,98 @@
 import bpy
+from bpy.props import BoolProperty
 import bmesh
 from math import degrees
 from .. utils.mesh import unhide_deselect, join
 from .. utils.object import flatten
+from .. utils.ui import popup_message
 
 
 class MeshCut(bpy.types.Operator):
     bl_idname = "machin3.mesh_cut"
     bl_label = "MACHIN3: Mesh Cut"
-    bl_description = "Knife Intersect a mesh, using another object.\nALT: flatten target object's modifier stack\nSHIFT: Mark Seam"
+    bl_description = "Cut a Mesh Object, using another Object.\nALT: Flatten Target Object's Modifier Stack\nSHIFT: Mark Seams"
     bl_options = {'REGISTER', 'UNDO'}
+
+    flatten_target: BoolProperty(name="Flatte Target's Modifier Stack", default=False)
+    mark_seams: BoolProperty(name="Mark Seams", default=False)
 
     @classmethod
     def poll(cls, context):
-        return context.mode == 'OBJECT' and len(context.selected_objects) == 2 and context.active_object and context.active_object in context.selected_objects and all(obj.type == 'MESH' for obj in context.selected_objects)
+        if context.mode == 'OBJECT':
+            return context.active_object and context.active_object.type == 'MESH'
+
+    def draw(self, context):
+        layout = self.layout
+        column = layout.column()
+
+        row = column.row(align=True)
+        row.prop(self, 'flatten_target', text="Flatten Target", toggle=True)
+        row.prop(self, 'mark_seams', toggle=True)
 
     def invoke(self, context, event):
+        self.flatten_target = event.alt
+        self.mark_seams = event.shift
+        return self.execute(context)
+
+    def execute(self, context):
         target = context.active_object
-        cutter = [obj for obj in context.selected_objects if obj != target][0]
+        cutters = [obj for obj in context.selected_objects if obj != target]
 
-        # unhide both
-        unhide_deselect(target.data)
-        unhide_deselect(cutter.data)
+        if cutters:
+            cutter = cutters[0]
 
-        # get depsgraph
-        dg = context.evaluated_depsgraph_get()
+            # unhide both
+            unhide_deselect(target.data)
+            unhide_deselect(cutter.data)
 
-        # flatten the cutter
-        flatten(cutter, dg)
+            # get depsgraph
+            dg = context.evaluated_depsgraph_get()
 
-        # flatten the target
-        if event.alt:
-            flatten(target, dg)
+            # always flatten the cutter
+            flatten(cutter, dg)
 
-        # clear cutter materials
-        cutter.data.materials.clear()
+            # optionally flatten the target
+            if self.flatten_target:
+                flatten(target, dg)
 
-        # join target and cutter
-        join(target, [cutter], select=[1])
+            # clear cutter materials
+            cutter.data.materials.clear()
 
-        # knife intersect
-        bpy.ops.object.mode_set(mode='EDIT')
-        if event.shift:
+            # join target and cutter
+            join(target, [cutter], select=[1])
+
+            # knife intersect, and separete the pieces, which will allow us to find and mark the new edges
+            bpy.ops.object.mode_set(mode='EDIT')
             bpy.ops.mesh.intersect(separate_mode='ALL')
-        else:
-            bpy.ops.mesh.intersect(separate_mode='CUT')
-        bpy.ops.object.mode_set(mode='OBJECT')
+            bpy.ops.object.mode_set(mode='OBJECT')
 
-        # remove cutter
-        bm = bmesh.new()
-        bm.from_mesh(target.data)
-        bm.normal_update()
-        bm.verts.ensure_lookup_table()
+            # remove cutter
+            bm = bmesh.new()
+            bm.from_mesh(target.data)
+            bm.normal_update()
+            bm.verts.ensure_lookup_table()
 
-        select_layer = bm.faces.layers.int.get('Machin3FaceSelect')
+            select_layer = bm.faces.layers.int.get('Machin3FaceSelect')
 
-        meshcut_layer = bm.edges.layers.int.get('Machin3EdgeMeshCut')
+            meshcut_layer = bm.edges.layers.int.get('Machin3EdgeMeshCut')
 
-        if not meshcut_layer:
-            meshcut_layer = bm.edges.layers.int.new('Machin3EdgeMeshCut')
+            if not meshcut_layer:
+                meshcut_layer = bm.edges.layers.int.new('Machin3EdgeMeshCut')
 
-        cutter_faces = [f for f in bm.faces if f[select_layer] > 0]
-        bmesh.ops.delete(bm, geom=cutter_faces, context='FACES')
+            cutter_faces = [f for f in bm.faces if f[select_layer] > 0]
+            bmesh.ops.delete(bm, geom=cutter_faces, context='FACES')
 
-        # mark seams
-        if event.shift:
+            # mark mesh cut edges
             non_manifold = [e for e in bm.edges if not e.is_manifold]
 
             # mark them and collect the verts as well
             verts = set()
 
             for e in non_manifold:
-                e.seam = True
                 e[meshcut_layer] = 1
+
+                if self.mark_seams:
+                    e.seam = True
 
                 verts.update(e.verts)
 
@@ -98,10 +118,13 @@ class MeshCut(bpy.types.Operator):
             # dissolve them
             bmesh.ops.dissolve_verts(bm, verts=straight_edged)
 
-        # remove face selection layer, it's no longer needed
-        bm.faces.layers.int.remove(select_layer)
+            # remove face selection layer, it's no longer needed now, keep the meshcut layer though
+            bm.faces.layers.int.remove(select_layer)
 
-        bm.to_mesh(target.data)
-        bm.clear()
+            bm.to_mesh(target.data)
+            bm.free()
 
-        return {'FINISHED'}
+            return {'FINISHED'}
+        else:
+            popup_message("Select one object first, then select the object to be cut last!", title="Illegal Sellection")
+            return {'CANCELLED'}
